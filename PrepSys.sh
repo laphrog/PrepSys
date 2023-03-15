641197d4323aceac0d31f4e569df2c5b1979231f
FreddyKrum <45272005+TheOldGoldenFish@users.noreply.github.com>
2023-03-15 16:04:27 +0330

    Update PrepSys.sh

--- PrepSys.sh
+++ PrepSys.sh
@@ -19,3 +19,11 @@ systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Install Snapd and CertBot
+
+yum install snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+certbot certonly --standalone
